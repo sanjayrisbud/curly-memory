108d2ebde5a6f72743bc5850e41ed15b7899589c
Sanjay Risbud <sanj19972001@yahoo.com>
2022-05-14 19:12:48 +0800

    added reports_creator.py for creation of reports

--- main/reports_creator.py
+++ main/reports_creator.py
@@ -3,3 +3,10 @@
 
 class ReportsCreator:
     """Class used to create the financial reports."""
+
+    def __init__(self, statement, db_engine):
+        self.statement = statement
+        self.db_engine = db_engine
+
+    def run(self):
+        """Perform class logic."""
--- tests/test_reports_creator.py
+++ tests/test_reports_creator.py
@@ -1,2 +1,14 @@
 """Tests for the ReportsCreator class."""
+from datetime import datetime
+from main.file_processor import FileProcessor
 from main.reports_creator import ReportsCreator
+
+
+def test_run(writable_path, engine):
+    """Unit test for run()."""
+    creator = ReportsCreator(
+        FileProcessor("dummy.xlsx", writable_path, datetime(2000, 1, 1)), engine
+    )
+    creator.run()
+    assert creator.statement.file_object.exists()
+    creator.statement.file_object.unlink()
