bf0c02956c32b391c9ec200c6c4ac15c675facf4
Sanjay Risbud <sanj19972001@yahoo.com>
2022-06-26 01:29:01 +0800

    refactored database saving to db_interface.py and adjusted fields to read data used to write financial statement

--- main/statement_writer.py
+++ main/statement_writer.py
@@ -3,39 +3,36 @@ from openpyxl import load_workbook
 from openpyxl.styles import NamedStyle, PatternFill, Border, Side, Alignment, Font
 from openpyxl.styles.borders import BORDER_NONE, BORDER_THIN
 from openpyxl.utils import get_column_letter
-from models.summary import Summary
 
 
 class StatementWriter:
-    """Class used to write out the financial statement."""
+    """Class used to write out the financial _statement."""
 
-    def __init__(self, statement, db_engine):
-        self.statement = statement
-        self.db_engine = db_engine
-        self.data = None
-        self.row = 1
+    def __init__(self, statement):
+        self._statement = statement
+        self._financial_data = None
+        self._row = 1
 
     def run(self, data):
         """Perform class logic."""
-        self.data = data
-        template = load_workbook(self.statement.file_object.parent / "template.xlsx")
+        self._financial_data = data
+        template = load_workbook(self._statement.file_object.parent / "template.xlsx")
         self.register_styles(template)
         assets = template.create_sheet("Assets")
         liabilities = template.create_sheet("Liabilities")
 
         self.populate_summary_sheet(template["Summary"])
         self.create_assets_sheet(assets)
-        self.row = 1
+        self._row = 1
         self.create_liabilities_sheet(liabilities)
 
-        self.store_in_database()
-        template.save(self.statement.file_object)
+        template.save(self._statement.file_object)
 
     def populate_summary_sheet(self, sheet):
-        """Populate summary sheet with the mined data."""
+        """Populate summary sheet with the mined _financial_data."""
         sheet["A2"] = "Sanjay Risbud"
-        sheet["A3"] = self.statement.date.strftime("%B %Y")
-        sheet["B26"] = self.statement.date.strftime("%m/%d/%Y")
+        sheet["A3"] = self._statement.date.strftime("%B %Y")
+        sheet["B26"] = self._statement.date.strftime("%m/%d/%Y")
 
         relevant_cells = {
             "BankAccounts": "B6",
@@ -43,102 +40,75 @@ class StatementWriter:
             "MutualFunds": "B9",
             "InsurancePolicies": "B11",
             "RealEstate": "B12",
+            "CreditCard": "B16",
             "AmortizationSchedule": "B18",
         }
-        summary_entries = []
 
-        for asset_class in self.data.get("asset_parsers", []):
-            type_ = asset_class.__class__.__name__.replace("Parser", "")
-            sheet[relevant_cells[type_]] = asset_class.total_amount
-            summary_entries.append(
-                Summary(self.statement.date, "ASSET", type_, asset_class.total_amount)
-            )
-
-        for liability_class in self.data.get("liability_parsers", []):
-            type_ = liability_class.__class__.__name__.replace("Parser", "")
-            sheet[relevant_cells[type_]] = liability_class.total_amount
-            summary_entries.append(
-                Summary(
-                    self.statement.date,
-                    "LIABILITY",
-                    type_,
-                    liability_class.total_amount,
-                )
-            )
-
-        if self.data.get("credit_card", None):
-            total_amount = self.data["credit_card"].balance
-            sheet["B16"] = total_amount
-            summary_entries.append(
-                Summary(self.statement.date, "LIABILITY", "CreditCard", total_amount)
-            )
-
-        self.data["summary_entries"] = summary_entries
+        for record in self._financial_data[0]:
+            sheet[relevant_cells[record.title]] = record.value
 
     def create_assets_sheet(self, sheet):
-        """Create assets sheet with the mined data."""
-        if not self.data.get("asset_parsers", None):
+        """Create assets sheet."""
+        assets = self._financial_data[1]
+        if not assets:
             return
-        self.write_bank_accounts_info(sheet, self.data["asset_parsers"][0])
-        self.write_mutual_funds_and_bonds_info(sheet, self.data["asset_parsers"][3])
-        self.write_portfolio_info(sheet, self.data["asset_parsers"][1])
-        self.write_life_insurance_info(sheet, self.data["asset_parsers"][2])
-        self.write_real_estate_info(sheet, self.data["asset_parsers"][4])
+        self.write_bank_accounts_info(sheet, assets.get("BankAccounts"))
+        self.write_mutual_funds_and_bonds_info(sheet, assets.get("MutualFunds"))
+        self.write_portfolio_info(sheet, assets.get("Portfolio"))
+        self.write_life_insurance_info(sheet, assets.get("InsurancePolicies"))
+        self.write_real_estate_info(sheet, assets.get("RealEstate"))
         self.adjust_column_widths(sheet)
 
     def create_liabilities_sheet(self, sheet):
-        """Create liabilities sheet with the mined data."""
-        if not self.data.get("credit_card", None):
+        """Create liabilities sheet."""
+        liabilities = self._financial_data[2]
+        if not liabilities:
             return
-
-        self.write_credit_card_info(sheet, self.data["credit_card"])
-        amortizations = self.data.get("liability_parsers", [])
-        if len(amortizations) == 1:
-            self.write_current_loan_balance(sheet, amortizations[0])
+        self.write_credit_card_info(sheet, liabilities["CreditCard"])
+        self.write_current_loan_balance(sheet, liabilities["AmortizationSchedule"])
         self.adjust_column_widths(sheet)
 
-    def write_bank_accounts_info(self, sheet, *args):
+    def write_bank_accounts_info(self, sheet, content):
         """Write out the information about bank accounts."""
         title = "Bank Accounts"
         header = ["Account Number", "Account Alias", "Bank", "Balance"]
         entries = [
             [record.account_number, record.account_alias, record.bank, record.balance]
-            for record in args[0].parsed_data
-            if record.account_alias != "MasterCard"
+            for record in content.get("records", [])
         ]
-        self.create_section(sheet, title, header, entries, args[0].total_amount)
+        self.create_section(sheet, title, header, entries, content.get("total_amount", ""))
 
-    def write_mutual_funds_and_bonds_info(self, sheet, *args):
+    def write_mutual_funds_and_bonds_info(self, sheet, content):
         """Write out the information about mutual funds and bonds."""
         title = "Securities: bonds / mutual funds"
         header = ["Name of Security", "Company", "Market Value"]
         entries = [
             [record.stock, "Sunlife", record.mkt_value]
-            for record in args[0].parsed_data
+            for record in content.get("records", [])
         ]
-        self.create_section(sheet, title, header, entries, args[0].total_amount)
+        self.create_section(sheet, title, header, entries, content.get("total_amount", ""))
 
-    def write_portfolio_info(self, sheet, *args):
+    def write_portfolio_info(self, sheet, content):
         """Write out the information about stock portfolio."""
         title = "Stock in privately held companies"
         header = ["Name of Security", "Number of Shares", "Market Value"]
         entries = [
             [record.stock, record.shares, record.mkt_value]
-            for record in args[0].parsed_data
+            for record in content.get("records", [])
         ]
-        self.create_section(sheet, title, header, entries, args[0].total_amount)
+        self.create_section(sheet, title, header, entries, content.get("total_amount", ""))
 
-    def write_life_insurance_info(self, sheet, *args):
+    def write_life_insurance_info(self, sheet, content):
         """Write out the information about life insurance policies."""
         title = "Life Insurance"
         header = ["Policy Name", "Company", "Policy Number", "Face Amount"]
         entries = [
             [record.account_alias, record.bank, record.account_number, record.balance]
-            for record in args[0].parsed_data
+            for record in content.get("records", [])
         ]
-        self.create_section(sheet, title, header, entries, args[0].total_amount)
+        self.create_section(sheet, title, header, entries, content.get("total_amount", ""))
 
-    def write_real_estate_info(self, sheet, *args):
+    def write_real_estate_info(self, sheet, content):
         """Write out the information about real estate."""
         title = "Real Estate"
         header = [
@@ -149,11 +119,11 @@ class StatementWriter:
         ]
         entries = [
             [record.address, record.price, record.year_bought, record.market_value]
-            for record in args[0].parsed_data
+            for record in content.get("records", [])
         ]
-        self.create_section(sheet, title, header, entries, args[0].total_amount)
+        self.create_section(sheet, title, header, entries, content.get("total_amount", ""))
 
-    def write_current_loan_balance(self, sheet, *args):
+    def write_current_loan_balance(self, sheet, content):
         """Write out the information about current loan balance."""
         title = "Mortgage / real estate loans payable"
         header = [
@@ -164,53 +134,42 @@ class StatementWriter:
         ]
         entries = [
             ["Philippine Savings Bank", 1_960_000, record.amount, record.balance]
-            for record in args[0].parsed_data
+            for record in content.get("records", [])
         ]
-        self.create_section(sheet, title, header, entries, args[0].total_amount)
+        self.create_section(sheet, title, header, entries, content.get("total_amount", ""))
 
-    def write_credit_card_info(self, sheet, *args):
+    def write_credit_card_info(self, sheet, content):
         """Write out the information about the credit card."""
         title = "Credit card & charge card debt"
         header = ["Account Number", "Bank", "Balance"]
-        entries = [[args[0].account_number, args[0].bank, args[0].balance]]
-        self.create_section(sheet, title, header, entries, args[0].balance)
+        entries = [[record.account_number, record.bank, record.balance]
+                   for record in content.get("records", [])
+        ]
+        self.create_section(sheet, title, header, entries, content.get("total_amount", ""))
 
     def create_section(self, sheet, *args):
         """Create a section of the sheet."""
         title, header, entries, total = args
-        sheet.cell(row=self.row, column=1, value=title).style = "title_style"
+        sheet.cell(row=self._row, column=1, value=title).style = "title_style"
 
-        self.row += 1
+        self._row += 1
         for column, field in enumerate(header, start=1):
-            sheet.cell(row=self.row, column=column, value=field).style = "header_style"
+            sheet.cell(row=self._row, column=column, value=field).style = "header_style"
 
         i = 0
         for i, record in enumerate(entries, start=1):
             for column, field in enumerate(record, start=1):
                 sheet.cell(
-                    self.row + i, column=column, value=field
+                    self._row + i, column=column, value=field
                 ).style = "entry_style"
 
-        self.row += i + 1
-        sheet.cell(row=self.row, column=1, value="Total").style = "total_title_style"
+        self._row += i + 1
+        sheet.cell(row=self._row, column=1, value="Total").style = "total_title_style"
         for i in range(2, len(header)):
-            sheet.cell(row=self.row, column=i).style = "total_blank_cell"
-        sheet.cell(row=self.row, column=i + 1, value=total).style = "total_value_style"
+            sheet.cell(row=self._row, column=i).style = "total_blank_cell"
+        sheet.cell(row=self._row, column=i + 1, value=total).style = "total_value_style"
 
-        self.row += 2
-
-    def store_in_database(self):
-        """Store various data points in the database for later use."""
-        summary_entries = self.data.get("summary_entries", None)
-        if not summary_entries:
-            return
-        Summary.insert_many(self.db_engine, summary_entries)
-        for bank_account in self.data["asset_parsers"][0].parsed_data:
-            if not bank_account.is_asset():
-                continue
-            bank_account.insert(self.db_engine)
-        for stock in self.data["asset_parsers"][1].parsed_data:
-            stock.insert(self.db_engine)
+        self._row += 2
 
     @staticmethod
     def register_styles(workbook):
