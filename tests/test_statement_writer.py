4883ec4961d506a9dab013c7c166ccd3433dd51d
Sanjay Risbud <sanj19972001@yahoo.com>
2022-06-26 02:00:04 +0800

    updated test file for new method signatures

--- tests/test_statement_writer.py
+++ tests/test_statement_writer.py
@@ -1,29 +1,29 @@
 """Tests for the StatementWriter class."""
 from datetime import datetime
+
 from openpyxl import Workbook
 
 from main.file_processor import FileProcessor
 from main.statement_writer import StatementWriter
 
 
-def test_run(writable_path, engine):
+def test_run(writable_path):
     """Unit test for run()."""
     writer = StatementWriter(
-        FileProcessor("dummy.xlsx", writable_path, datetime(2000, 1, 1)), engine
-    )
-    writer.run({})
-    assert writer.statement.file_object.exists()
-    writer.statement.file_object.unlink()
+        FileProcessor("dummy.xlsx", writable_path, datetime(2000, 1, 1)))
+    writer.run(([], {}, {}))
+    assert writer._statement.file_object.exists()
+    writer._statement.file_object.unlink()
 
 
-def test_populate_summary_sheet(writable_path, engine):
+def test_populate_summary_sheet(writable_path):
     """Unit test for populate_summary_sheet()."""
     stmt = StatementWriter(
-        FileProcessor("dummy.xlsx", writable_path, datetime(2000, 1, 1)), engine
+        FileProcessor("dummy.xlsx", writable_path, datetime(2000, 1, 1))
     )
     workbook = Workbook()
     worksheet = workbook.create_sheet()
-    stmt.data = {}
+    stmt._financial_data = [], {}, {}
     stmt.populate_summary_sheet(worksheet)
     assert worksheet["A3"].value == "January 2000"
     workbook.close()
