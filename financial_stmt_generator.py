09ac0fa766eeb4e94524b8001ad12289d948361b
Sanjay Risbud <sanj19972001@yahoo.com>
2022-06-26 01:55:35 +0800

    created new methods to consolidate data to be read by statement_writer.py and reports_creator.py

--- financial_stmt_generator.py
+++ financial_stmt_generator.py
@@ -3,9 +3,12 @@ import argparse
 from datetime import datetime
 
 import models
+from main.db_interface import DatabaseInterface
 from main.file_processor import FileProcessor
 from main.reports_creator import ReportsCreator
 from main.statement_writer import StatementWriter
+from models.bank_account import BankAccount
+from models.summary import Summary
 from parsers.amortization_schedule_parser import AmortizationScheduleParser
 from parsers.bank_accounts_parser import BankAccountsParser
 from parsers.insurance_policies_parser import InsurancePoliciesParser
@@ -15,7 +18,7 @@ from parsers.real_estate_parser import RealEstateParser
 
 
 class FinancialStatementGenerator:
-    """Class to generate financial statement."""
+    """Class to generate financial _statement."""
 
     def __init__(self, environment):
         path = "C:/Users/sanjay s risbud/Dropbox/statements"
@@ -25,13 +28,13 @@ class FinancialStatementGenerator:
         self.statement = FileProcessor(
             "financial_statement.xlsx", path_for_writes, date
         )
-        self.db_file = FileProcessor(
+        db_file = FileProcessor(
             "db.sqlite3",
             path=path_for_writes,
             date=date,
             archive_path=self.statement.archive_path,
         )
-        self.db_engine = models.get_engine(path_for_writes + "/db.sqlite3")
+        db_engine = models.get_engine(path_for_writes + "/db.sqlite3")
 
         self.asset_parsers = [
             BankAccountsParser("BPI Online.pdf", path, date),
@@ -47,21 +50,22 @@ class FinancialStatementGenerator:
             AmortizationScheduleParser("loan.html", path, date),
         ]
 
-        self.statement_writer = StatementWriter(self.statement, self.db_engine)
-        self.reports_creator = ReportsCreator(self.statement, self.db_engine)
+        self.statement_writer = StatementWriter(self.statement)
+        self.reports_creator = ReportsCreator(self.statement)
+        self.db_interface = DatabaseInterface(db_file, db_engine)
 
     def run(self, environment):
         """Run the application logic."""
-        self.mine_data()
+        data = self.mine_data()
         if environment != "dev":
-            self.write_statement()
+            self.statement_writer.run(data)
         else:
-            self.reports_creator.run()
+            self.reports_creator.run(data)
         self.statement.archive()
-        self.db_file.archive()
+        self.db_interface.archive_db_file()
 
     def mine_data(self):
-        """Mine the data to be used to populate the statement."""
+        """Mine the _financial_data to be used to populate the _statement."""
         for parser in self.asset_parsers:
             parser.get_data()
             if parser.extractor:
@@ -70,17 +74,53 @@ class FinancialStatementGenerator:
         for parser in self.liability_parsers:
             parser.get_data()
 
-    def write_statement(self):
-        """Write out the financial statement, sourcing the mined data."""
-        credit_card_list = [
-            account
-            for account in self.asset_parsers[0].parsed_data
-            if not account.is_asset()
-        ]
-        credit_card = credit_card_list[0] if len(credit_card_list) == 1 else None
-        data = vars(self)
-        data["credit_card"] = credit_card
-        self.statement_writer.run(data)
+        return self.consolidate_financial_data()
+
+    def consolidate_financial_data(self):
+        """Consolidate the _financial_data from the asset and liability parsers."""
+        assets = {}
+        for parser in self.asset_parsers:
+            assets[parser.__class__.__name__.replace("Parser", "")] = {
+                "records": parser.parsed_data,
+                "total_amount": parser.total_amount
+            }
+        liabilities = {}
+        for parser in self.liability_parsers:
+            liabilities[parser.__class__.__name__.replace("Parser", "")] = {
+                "records": parser.parsed_data,
+                "total_amount": parser.total_amount
+            }
+        self.move_credit_cards_to_liabilities(assets, liabilities)
+        summary = self.generate_summary_entries(assets, liabilities, self.statement.date)
+        return summary, assets, liabilities
+
+    @staticmethod
+    def move_credit_cards_to_liabilities(assets, liabilities):
+        """Move the credit cards (if any) from to liabilities."""
+        credit_cards = []
+        to_delete = []
+        total_amount = 0
+        for account in assets.get("BankAccounts", {}).get("records", {}):
+            if not account.is_asset():
+                total_amount += account.balance
+                card = BankAccount(account.date, account.bank, account.account_alias,
+                                   account.account_number, account.balance)
+                to_delete.append(account)
+                credit_cards.append(card)
+
+        for account in to_delete:
+            assets["BankAccounts"]["records"].remove(account)
+        liabilities["CreditCard"] = {"records": credit_cards, "total_amount": total_amount}
+
+    @staticmethod
+    def generate_summary_entries(assets, liabilities, date):
+        """Generate a summary of asset and liability entries."""
+        summary = []
+        for type_, asset in assets.items():
+            summary.append(Summary(date, "ASSET", type_, asset["total_amount"]))
+        for type_, liability in liabilities.items():
+            summary.append(Summary(date, "LIABILITY", type_, liability["total_amount"]))
+        return summary
 
 
 def main():
