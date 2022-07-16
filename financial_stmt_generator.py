3d3aa356f57fea2833da5846eb4dae08ea17b247
Sanjay Risbud <sanj19972001@yahoo.com>
2022-07-16 21:48:54 +0800

    added statement for reports creation to only be done on dev

--- financial_stmt_generator.py
+++ financial_stmt_generator.py
@@ -57,9 +57,8 @@ class FinancialStatementGenerator:
     def run(self, environment):
         """Run the application logic."""
         data = self.mine_data()
-        if environment != "dev":
-            self.statement_writer.run(data)
-        else:
+        self.statement_writer.run(data)
+        if environment == "dev":
             self.reports_creator.run(data)
         self.db_interface.store_various_financial_data(data)
         self.statement.archive()
