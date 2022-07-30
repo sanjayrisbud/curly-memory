53b0405dbc0f93d3bbed4909f56a53414d3c1842
Sanjay Risbud <sanj19972001@yahoo.com>
2022-07-31 00:02:24 +0800

    refactored code that creates new charts

--- main/reports_creator.py
+++ main/reports_creator.py
@@ -45,9 +45,16 @@ class ReportsCreator:
             )
         else:
             workbook = load_workbook(file_object)
-        for chart in self._charts_to_create:
-            sheet = workbook.create_sheet(title=chart[0])
-            image = chart[1](self.financial_data, self._db_interface).get_image()
-            image.anchor = "A1"
-            sheet.add_image(image)
+        self.add_charts_to_workbook(workbook)
         workbook.save(file_object)
+
+    def add_charts_to_workbook(self, workbook):
+        """Add needed charts to the workbook."""
+        for chart in self._charts_to_create:
+            if chart[0] in workbook.get_sheet_names():
+                sheet = workbook.get_sheet_by_name(chart[0])
+            else:
+                sheet = workbook.create_sheet(title=chart[0])
+            image = chart[1](self.financial_data, self._db_interface).get_image()
+            image.anchor = "C1"
+            sheet.add_image(image)
