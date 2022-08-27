19b37101cf14efddd3dc5ae325b0b87a14b16359
Sanjay Risbud <sanj19972001@yahoo.com>
2022-08-27 16:53:43 +0800

    corrected tests of reports_creator.py

--- tests/main/test_reports_creator.py
+++ tests/main/test_reports_creator.py
@@ -1,6 +1,5 @@
 """Tests for the ReportsCreator class."""
 from datetime import datetime
-
 import models
 from main.db_interface import DatabaseInterface
 from main.file_processor import FileProcessor
@@ -10,7 +9,11 @@ from main.reports_creator import ReportsCreator
 def test_run(writable_path):
     """Unit test for run()."""
     statement = FileProcessor("dummy.xlsx", writable_path, datetime(2000, 1, 1))
-    creator = ReportsCreator(statement)
+    db_file = FileProcessor("db.sqlite3", path=writable_path, date=None)
+    db_engine = models.get_engine(writable_path + "/db.sqlite3")
+    creator = ReportsCreator(
+        statement=statement, db_interface=DatabaseInterface(db_file, db_engine)
+    )
     creator.run(([], {}, {}))
     assert creator.statement.file_object.exists()
     creator.statement.file_object.unlink()
@@ -22,9 +25,3 @@ def test_get_previous_financial_data(writable_path):
         FileProcessor("dummy.xlsx", writable_path, datetime(2000, 1, 1))
     )
     assert creator.get_previous_financial_data()[0] == []
-
-    db_file = FileProcessor("db.sqlite3", path=writable_path, date=None)
-    db_engine = models.get_engine(writable_path + "/db.sqlite3")
-    creator = ReportsCreator(
-        statement=creator.statement, db_interface=DatabaseInterface(db_file, db_engine)
-    )
