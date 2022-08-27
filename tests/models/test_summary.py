8a1e2f3ff1c9405b489701a925892f1ec1ba58ef
Sanjay Risbud <sanj19972001@yahoo.com>
2022-08-27 16:43:07 +0800

    modified tests of summary.py as they keep inserting rouge records to database

--- tests/models/test_summary.py
+++ tests/models/test_summary.py
@@ -5,32 +5,37 @@ from models.summary import Summary
 
 def test_insert(engine):
     """Unit test for insert()."""
-    record = Summary(datetime.today(), "ASSET", "a", 23)
+    date = datetime(2019, 5, 3)
+    record = Summary(date, "ASSET", "a", 23)
+    record_id = record.insert(engine)
+    assert record_id > 0
+    record = Summary(date, "LIABILITY", "b", 24)
     record_id = record.insert(engine)
     assert record_id > 0
 
 
 def test_insert_many(engine):
     """Unit test for insert_many()."""
+    date = datetime(2019, 6, 3)
     records = [
-        Summary(datetime.today(), "Z", "a", 23),
-        Summary(datetime.today(), "Y", "b", 24),
+        Summary(date, "ASSET", "a", 23),
+        Summary(date, "LIABILITY", "b", 24),
     ]
     record_ids = Summary.insert_many(engine, records)
-    assert len(record_ids) > 0 and all(record_ids)
+    assert len(record_ids) % 2 == 0 and all(record_ids)
 
 
 def test_find_by_date(engine):
     """Unit test for find_by_date()."""
-    date = datetime.today()
+    date = datetime(2019, 7, 3)
     records = [
         Summary(date, "LIABILITY", "c", 1.5),
-        Summary(datetime(2019, 6, 3), "ASSET", "x", -1),
+        Summary(date, "ASSET", "x", -1),
         Summary(date, "LIABILITY", "d", 2.4),
     ]
     Summary.insert_many(engine, records)
     found = Summary.find_by_date(engine, date)
-    assert len([1 for row in found]) == 2
+    assert len([1 for _ in found]) % 3 == 0
 
 
 def test_str():
